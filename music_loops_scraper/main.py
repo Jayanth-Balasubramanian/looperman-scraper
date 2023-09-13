8a1fbd489b4e6d4573b86d72d999f2faac596c86
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2023-09-13 23:53:00 +0800

    Remove prints, scrape 30k items

--- music_loops_scraper/main.py
+++ music_loops_scraper/main.py
@@ -1,8 +1,12 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from spiders.loops_spider import LoopSpider
+from time import perf_counter
 
 if __name__ == "__main__":
     process = CrawlerProcess(get_project_settings())
     process.crawl(LoopSpider)
+    start_time = perf_counter()
     process.start()
+    end_time = perf_counter()
+    print(f"Elapsed time: {end_time - start_time}")
--- music_loops_scraper/spiders/loops_spider.py
+++ music_loops_scraper/spiders/loops_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from items import MusicLoopItem, CategoryLoopsItem
+from math import ceil
 
 
 class LoopSpider(scrapy.Spider):
@@ -23,7 +24,7 @@ class LoopSpider(scrapy.Spider):
         "Violin",
     }
     # allowed_cats = {"Bass", "Bass Guitar", "Bass Synth"}
-    MAX_LOOPS_TO_SCRAPE = 100
+    MAX_LOOPS_TO_SCRAPE = 30000
 
     def __init__(self):
         super().__init__()
@@ -38,10 +39,7 @@ class LoopSpider(scrapy.Spider):
             cat_id = cat.css("::attr(value)").get()
 
             if cat_name in LoopSpider.allowed_cats:
-                print(cat_name)
-
                 cat_url = f"https://www.looperman.com/loops?page=1&cid={cat_id}"
-                print(f"yielded cat url {cat_url}")
                 yield scrapy.Request(
                     url=cat_url,
                     callback=self.parse_cat,
@@ -58,28 +56,21 @@ class LoopSpider(scrapy.Spider):
         player_wrappers = response.css("div.player-wrapper")
         description_wrappers = response.css("div.desc-wrapper")
         tag_wrappers = response.css("div.tag-wrapper")
-        max_count_for_cat = int(
-            (self.num_cats_scraped + 1)
-            * LoopSpider.MAX_LOOPS_TO_SCRAPE
-            / len(LoopSpider.allowed_cats)
+        max_count_for_cat = self.num_loops_scraped + ceil(
+            (LoopSpider.MAX_LOOPS_TO_SCRAPE - self.num_loops_scraped)
+            / (len(LoopSpider.allowed_cats) - self.num_cats_scraped)
         )
-        print(f"max count for cat {max_count_for_cat}")
+
         for i, player_wrapper in enumerate(player_wrappers):
             if self.num_loops_scraped >= max_count_for_cat:
                 break
             loop_name = player_wrapper.css("a.player-title::text")
-            # pprint(player_wrapper.get())
-            print(f"loop name {loop_name.get()}")
             url = player_wrapper.css("::attr(rel)")
-            print(f"url {url.get()}")
             description = description_wrappers[i].css("p::text")
-            print(f"description {description.get()}")
             genre = tag_wrappers[i].css('a[title^="Genre"]::text')
-            print(f"genre {genre.get()}")
             bpm = tag_wrappers[i].css('a[title^="Find more loops at"]::text')
-            print(f"bpm {bpm.get()}")
             key = tag_wrappers[i].css('a[title^="This loop is in the key of"]::text')
-            print(f'key {"Unknown" if not key else key.get().split()[-1]}')
+
             this_loop_data = MusicLoopItem(
                 name=loop_name.get(),
                 url=url.get(),
@@ -97,13 +88,11 @@ class LoopSpider(scrapy.Spider):
 
             this_category_loops.loops.append(this_loop_data)
             self.num_loops_scraped += 1
-        print(f"num loops scraped {self.num_loops_scraped}")
         next_page = response.css(
             'div.pagination-links a:contains(">")::attr(href)'
         ).get()
         should_go_next = next_page and self.num_loops_scraped < max_count_for_cat
         if should_go_next:
-            print("going to next page")
             yield response.follow(
                 next_page, callback=self.parse_cat, meta={"cat_name": cat_name}
             )
